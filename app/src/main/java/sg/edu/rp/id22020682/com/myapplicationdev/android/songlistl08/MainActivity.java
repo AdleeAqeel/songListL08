25287da4447ea7bf21c5e666b36de80c09d3c325
AdleeAqeel <123053542+AdleeAqeel@users.noreply.github.com>
2023-07-03 15:16:57 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/id22020682/com/myapplicationdev/android/songlistl08/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.id22020682.com.myapplicationdev.android.songlistl08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
